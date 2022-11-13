8c72dcdb44b3b42b2cf9468da41d9b0adb7a7764
Zain Adam <zainadam.id@gmail.com>
2022-11-13 22:08:49 +0700

    chore: fixing initial version

--- entrypoints/db_checks.php
+++ entrypoints/db_checks.php
@@ -1,13 +1,13 @@
 <?php
 
-$APP_NAME = $argv[1];
+$USER = $argv[1];
 $ROOT_PASSWORD = $argv[2];
 $MYSQL_USER = $argv[3];
 $DATABASE_NAME = $argv[4];
 
-$dbh = new PDO("mysql:host=$APP_NAME-mysql", 'root', $ROOT_PASSWORD);
+$dbh = new PDO("mysql:host=mysql", 'root', $ROOT_PASSWORD);
 
-$databases = explode(' ', $DATABASE_NAME);
+$databases = explode("\n", $DATABASE_NAME);
 
 $query = '';
 $where = [];
@@ -21,7 +21,6 @@ foreach($databases as $dbName) {
 
 $query .= 'FLUSH PRIVILEGES';
 
-
 echo "creating and granting databases if is not exists\n";
 $dbh->exec($query);
 
@@ -30,7 +29,15 @@ $where = implode(' or ', $where);
 print_r("checking db table(s)\n");
 $nRows = $dbh->query("SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE $where")->fetchColumn();
 	
-if ($nRows < 2) {
+if ($nRows <= 1) {
 	print_r("migrating db\r\n");
 	passthru('php artisan migrate --force');
 }
+
+// check if migration is failed
+$nRows = $dbh->query("SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE $where")->fetchColumn();
+if ($nRows <= 1) {
+	throw new Exception("Migration Failed");
+}
+
+touch("/home/$USER/logs/DB_CHECKED");
--- entrypoints/php-entrypoint.sh
+++ entrypoints/php-entrypoint.sh
@@ -1,20 +1,109 @@
 #!/bin/sh
 set -e
 
-cd /var/www/$APP_NAME
+# Check if $USER is exists
+if [ ! -d /home/$USER/.composer ]; then
+	echo "Creating user $USER";
+	rm -r /var/www || true;
 
-if [ ! -s .env ] && [ -f .env_bak ]; then
-	cp .env_bak .env
-	rm .env_bak
+	# Create system user to run Composer and Artisan Commands
+	useradd -G www-data,root -u 1000 -d /home/$USER $USER;
+
+	mkdir -p /home/$USER/.composer;
+	mkdir -p /home/$USER/logs;
+	chown -R $USER:$USER /home/$USER;
 fi
 
-php /usr/local/bin/db_checks.php $APP_NAME $MYSQL_ROOT_PASSWORD $MYSQL_USER "$DATABASE_NAME"
+# Check if file composer.json is exists
+if [ ! -f "/var/www/composer.json" ]; then
+	APP_SRC_GIT=$(cat .env | grep "APP_SRC_GIT=" | grep -v "#" | tail -1 | cut -d '=' -f2);
+	APP_SRC_GIT_BRANCH=$(cat .env | grep "APP_SRC_GIT_BRANCH=" | grep -v "#" | tail -1 | cut -d '=' -f2);
 
-su - $APP_NAME
+
+	if [ ! -f "/home/$USER/app_src/composer.json" ] && [ ! "$APP_SRC_GIT" ]; then
+		echo "Application source is not found!";
+		exit 1;
+	fi
+
+	if [ -f "/home/$USER/app_src/composer.json" ] && [ "$APP_SRC_GIT" ]; then
+		echo "Application source is inconsitent!";
+		exit 1;
+	fi
+
+	# Copy from /home/$USER/app_src/composer.json
+	if [ -f "/home/$USER/app_src/composer.json" ]; then
+		echo "Copy Application from local";
+		cp /home/$USER/app_src /tmp/laravel-app;
+	fi
+
+	# Clone from git
+	if [ "$APP_SRC_GIT" ]; then
+		echo "Cloning Application from: $APP_SRC_GIT";
+
+		if [ "$APP_SRC_GIT_BRANCH" ]; then
+			echo "Branch: ${APP_SRC_GIT_BRANCH}";
+			git clone --branch $APP_SRC_GIT_BRANCH $APP_SRC_GIT /tmp/laravel-app;
+		else
+			git clone $APP_SRC_GIT /tmp/laravel-app;
+		fi
+	fi
+
+	cp -a /tmp/laravel-app/. /var/www/
+	rm -r /tmp/laravel-app
+
+	chown -R $USER:www-data /var/www;
+fi
+
+su - $USER
+
+cd /var/www
+
+chmod -R 775 bootstrap/cache
+chmod -R 775 storage
+
+if [ ! -d "vendor" ]; then
+	echo "Installing packages from composer";
+	composer install --optimize-autoloader --no-dev;
+fi
+
+php artisan storage:link
+
+DB_DATABASES=$(cat .env | grep DB_DATABASE | grep -v "#" | cut -d '=' -f2)
+if [ -z "$DB_DATABASES" ]; then
+	echo "Database name is set to forge because is empty";
+	DB_DATABASES=forge;
+fi
+
+
+# clear all caches first
+php artisan cache:clear
+rm -r bootstrap/cache/* 
+php artisan config:clear
+
+
+APP_KEY=$(cat .env | grep "APP_KEY=" | grep -v "#" | tail -1 | cut -d '=' -f2)
+if [ -z "$APP_KEY" ]; then
+	echo "APP_KEY not found";
+	echo "generating APP_KEY";
+	php artisan key:generate;
+fi
+
+if [ ! -e "/home/$USER/logs/DB_CHECKED" ]; then
+	DB_ROOT_PASSWORD=$(cat .env | grep DB_ROOT_PASSWORD= | grep -v "#" | tail -1 | cut -d '=' -f2);
+	DB_USERNAME=$(cat .env | grep DB_USERNAME= | grep -v "#" | tail -1 | cut -d '=' -f2);
+	DB_PASSWORD=$(cat .env | grep PASSWORD= | grep -v "#" | tail -1 | cut -d '=' -f2);
+
+	php /usr/local/bin/db_checks.php $USER $DB_ROOT_PASSWORD $DB_USERNAME "$DB_DATABASES";
+fi
+
+# (re)cache all for optimized
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache || true
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
+	set -- php-fpm "$@";
 fi
 
 exec "$@"
